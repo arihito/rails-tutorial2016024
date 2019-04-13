483efdbfb73d089ef5d5ab6e9f3aff563c8e86bb
マツダアリヒト <arihito.m@gmail.com>
2019-04-13 09:31:10 +0000

    first develop

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  
+  devise_for :users
+  devise_for :users
+  root to: "blogs#index"
+  
   resources :blogs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
